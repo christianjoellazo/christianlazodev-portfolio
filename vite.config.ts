b99a09b221bcbd2a2a8de010cece0cab22f114f6
Christian Lazo <lazo.c2001@gmail.com>
2025-05-20 09:56:11 +0800

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig, PluginOption } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+export default defineConfig(async ({ mode }) => {
+  // Dynamically build plugins array
+  const plugins: PluginOption[] = [react()];
+  if (mode === "development") {
+    // Use await import for ESM-only package
+    const { componentTagger } = await import("lovable-tagger");
+    plugins.push(componentTagger() as PluginOption);
+  }
+
+  return {
+    // Set base path for GitHub Pages
+    base: mode === "production" ? "/christianlazo-portfolio": "/",
+    server: {
+      host: "0.0.0.0",
+      port: 8080,
+    },
+    plugins,
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
